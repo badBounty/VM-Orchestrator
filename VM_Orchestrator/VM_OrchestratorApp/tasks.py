d9fbfaf8a6c420e8bf6946526dc48a61d951f582
handerllon <manuel121097@gmail.com>
2020-06-26 16:05:16 -0300

    Fix

--- VM_Orchestrator/VM_OrchestratorApp/tasks.py
+++ VM_Orchestrator/VM_OrchestratorApp/tasks.py
@@ -174,18 +174,18 @@ def run_web_scanners(scan_information):
         [
             # Fast_scans
             header_scan_task.s(web_information).set(queue='fast_queue'),
-            http_method_scan_task.s(web_information).set(queue='fast_queue'),
-            libraries_scan_task.s(web_information).set(queue='fast_queue'),
-            ffuf_task.s(web_information).set(queue='fast_queue'),
-            iis_shortname_scan_task.s(web_information).set(queue='fast_queue'),
-            bucket_finder_task.s(web_information).set(queue='fast_queue'),
-            token_scan_task.s(web_information).set(queue='fast_queue'),
-            css_scan_task.s(web_information).set(queue='fast_queue'),
-            firebase_scan_task.s(web_information).set(queue='fast_queue'),
-            host_header_attack_scan.s(web_information).set(queue='fast_queue'),
+            #http_method_scan_task.s(web_information).set(queue='fast_queue'),
+            #libraries_scan_task.s(web_information).set(queue='fast_queue'),
+            #ffuf_task.s(web_information).set(queue='fast_queue'),
+            #iis_shortname_scan_task.s(web_information).set(queue='fast_queue'),
+            #bucket_finder_task.s(web_information).set(queue='fast_queue'),
+            #token_scan_task.s(web_information).set(queue='fast_queue'),
+            #css_scan_task.s(web_information).set(queue='fast_queue'),
+            #firebase_scan_task.s(web_information).set(queue='fast_queue'),
+            #host_header_attack_scan.s(web_information).set(queue='fast_queue'),
             # Slow_scans
-            cors_scan_task.s(web_information).set(queue='slow_queue'),
-            ssl_tls_scan_task.s(web_information).set(queue='slow_queue'),
+            #cors_scan_task.s(web_information).set(queue='slow_queue'),
+            #ssl_tls_scan_task.s(web_information).set(queue='slow_queue'),
             #burp_scan_task.s(web_information).set(queue='slow_queue')
         ],
         body=web_security_scan_finished.s().set(queue='fast_queue'),
@@ -217,8 +217,8 @@ def run_ip_scans(scan_information):
     # We will flag the resource as scanned here, mainly because all alive resources will reach this point
     execution_chord = chord(
         [
-            nmap_script_baseline_task.s(ip_information).set(queue='slow_queue'),
-            nmap_script_scan_task.s(ip_information).set(queue='slow_queue'),
+            #nmap_script_baseline_task.s(ip_information).set(queue='slow_queue'),
+            #nmap_script_scan_task.s(ip_information).set(queue='slow_queue'),
             add_scanned_resources.s(ip_information).set(queue='fast_queue')
         ],
         body=ip_security_scan_finished.s().set(queue='fast_queue'),
