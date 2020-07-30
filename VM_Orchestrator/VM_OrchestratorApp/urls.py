bd8cabc37cbcca076f9a060c1e719917f385a1c6
Handerllon <manuel121097@gmail.com>
2020-07-30 14:20:29 -0300

    Fix

--- VM_Orchestrator/VM_OrchestratorApp/urls.py
+++ VM_Orchestrator/VM_OrchestratorApp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from . import views
+from VM_OrchestratorApp import views
 
 app_name = 'VM_OrchestratorApp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('start_recon/', views.run_recon_against_target, name='start_recon'),
-    path('get_resources/', views.get_resources_from_target, name='get_resources')
-    #path('on_demand_scan/', views.on_demand_scan, name='on_demand_scan')
+    path('get_resources/', views.get_resources_from_target, name='get_resources'),
+    path('on_demand_scan/', views.on_demand_scan, name='on_demand_scan')
 ]
