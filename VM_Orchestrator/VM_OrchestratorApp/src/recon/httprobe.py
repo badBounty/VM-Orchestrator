b193d1bb7d9bc868d4608f215075832f4d5e1153
Handerllon <manuel121097@gmail.com>
2020-08-04 12:25:33 -0300

    httprobe fix 2

--- VM_Orchestrator/VM_OrchestratorApp/src/recon/httprobe.py
+++ VM_Orchestrator/VM_OrchestratorApp/src/recon/httprobe.py
@@ -4,13 +4,13 @@ def start_httprobe(subdomain_list, scan_info):
     print('Starting httprobe against ' + str(len(subdomain_list))+ ' subdomains')
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     TOOL_DIR = ROOT_DIR + '/tools/httprobe'
-    OUTPUT_DIR = ROOT_DIR + '/output'
+    OUTPUT_DIR = ROOT_DIR + '/output/httprobe_input.txt'
     for subdomain in subdomain_list:
         print('Probing %s' % subdomain['subdomain'])
-        with open(OUTPUT_DIR+'/httprobe_input.txt', 'w') as f:
+        with open(OUTPUT_DIR, 'w') as f:
             f.write("%s\n" % subdomain['subdomain'])
 
-        output = os.popen('cat httprobe_input.txt | '+TOOL_DIR).read()
+        output = os.popen('cat ' + OUTPUT_DIR + ' | '+TOOL_DIR).read()
         print(output)
         output_list = output.split('\n')
         output_list.remove("")
@@ -18,7 +18,7 @@ def start_httprobe(subdomain_list, scan_info):
             print(item)
 
         try:
-            os.remove(OUTPUT_DIR+'/httprobe_input.txt')
+            os.remove(OUTPUT_DIR)
         except OSError:
             pass
 
