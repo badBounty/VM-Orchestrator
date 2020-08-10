d9e7aaf2e443bcf877bca99d9964a199964559fb
Handerllon <manuel121097@gmail.com>
2020-08-10 14:27:41 -0300

    Added update url

--- VM_Orchestrator/VM_OrchestratorApp/urls.py
+++ VM_Orchestrator/VM_OrchestratorApp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('on_demand_scan/', views.on_demand_scan, name='on_demand_scan'),
     path('update_elasticsearch/', views.force_update_elasticsearch, name='update_elasticsearch'),
     path('sync_redmine/', views.force_redmine_sync, name='sync_redmine'),
+    path('add_mongo_vulns_to_redmine/', views.add_mongo_vulns_to_redmine, name='add_mongo_vulns_to_redmine'),
     # Views
     path('', views.index, name='index'),
     path('test_html/', views.test_html, name='test_html'),
