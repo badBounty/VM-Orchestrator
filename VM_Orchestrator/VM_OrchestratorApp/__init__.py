6781f6d0363411c30f3b81df0894e4a5ae6d987a
Leonardo Marazzo <leonardojmarazzo@gmail.com>
2021-01-28 16:05:44 -0300

    Cred

--- VM_Orchestrator/VM_OrchestratorApp/__init__.py
+++ VM_Orchestrator/VM_OrchestratorApp/__init__.py
@@ -22,7 +22,7 @@ if settings['ELASTIC']['IP'] != '':
         )"""
         host = "{}:{}".format(settings['ELASTIC']['IP'],settings['ELASTIC']['PORT'])
         ELASTIC_CLIENT = Elasticsearch([host], use_ssl=True, ssl_assert_hostname=False,
-                        verify_certs=True, ca_certs=pathCertificate)
+                        verify_certs=True, ca_certs=pathCertificate, http_auth=(settings['ELASTIC']['USER'], settings['ELASTIC']['SECRET']))
 MONGO_CLIENT = pymongo.MongoClient(MONGO_INFO['CLIENT_URL'], connect=False)
 if settings['SLACK']['INTERNAL_SLACK_KEY'] != '':
     INTERNAL_SLACK_WEB_CLIENT = WebClient(settings['SLACK']['INTERNAL_SLACK_KEY'])
