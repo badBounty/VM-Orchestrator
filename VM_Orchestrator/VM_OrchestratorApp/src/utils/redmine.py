79a09aebdb5837fc322496154abc8979dd1dd6d0
Kalust <39742600+Kalust@users.noreply.github.com>
2021-08-26 10:49:18 -0300

    Update redmine.py

--- VM_Orchestrator/VM_OrchestratorApp/src/utils/redmine.py
+++ VM_Orchestrator/VM_OrchestratorApp/src/utils/redmine.py
@@ -106,33 +106,33 @@ def create_new_web_issue(vuln):
         return
     if issue_already_exists(vuln):
         return
-    issue = redmine_client.issue.new()
-    issue.project_id = settings['REDMINE']['project_name']
-    issue.subject = vuln.vulnerability_name
-    issue.tracker_id = REDMINE_IDS['WEB_FINDING']['FINDING_TRACKER']
-    issue.description = vuln.custom_description
-    issue.status_id = vuln.status
-    issue.priority_id = vuln.resolve_priority()
-    issue.assigned_to_id = REDMINE_IDS['ASSIGNED_USER']
-    issue.watcher_user_ids = REDMINE_IDS['WATCHERS']
-    issue.custom_fields= [
-    {'id': REDMINE_IDS['WEB_FINDING']['IDENTIFIER'], 'value': vuln.id},
-    {'id': REDMINE_IDS['WEB_FINDING']['DOMAIN'], 'value': vuln.domain},
-    {'id': REDMINE_IDS['WEB_FINDING']['RESOURCE'], 'value': vuln.target},
-    {'id': REDMINE_IDS['WEB_FINDING']['DATE_FOUND'], 'value': str(vuln.time.strftime("%Y-%m-%d"))},
-    {'id': REDMINE_IDS['WEB_FINDING']['LAST_SEEN'], 'value': str(vuln.time.strftime("%Y-%m-%d"))},
-    {'id': REDMINE_IDS['WEB_FINDING']['CVSS_SCORE'], 'value': vuln.cvss},
-    {'id': REDMINE_IDS['WEB_FINDING']['KB_DESCRIPTION'], 'value': str(vuln.observation.observation_title)},
-    {'id': REDMINE_IDS['WEB_FINDING']['KB_DESCRIPTION_NOTES'], 'value': str(vuln.observation.observation_note)},
-    {'id': REDMINE_IDS['WEB_FINDING']['KB_IMPLICATION'], 'value': str(vuln.observation.implication)},
-    {'id': REDMINE_IDS['WEB_FINDING']['KB_RECOMMENDATION'], 'value': str(vuln.observation.recommendation_title)},
-    {'id': REDMINE_IDS['WEB_FINDING']['KB_RECOMMENDATION_NOTES'], 'value': str(vuln.observation.recommendation_urls)}
-    ]
-    issue.uploads = vuln.attachments
     try:
+        issue = redmine_client.issue.new()
+        issue.project_id = settings['REDMINE']['project_name']
+        issue.subject = vuln.vulnerability_name
+        issue.tracker_id = REDMINE_IDS['WEB_FINDING']['FINDING_TRACKER']
+        issue.description = vuln.custom_description
+        issue.status_id = vuln.status
+        issue.priority_id = vuln.resolve_priority()
+        issue.assigned_to_id = REDMINE_IDS['ASSIGNED_USER']
+        issue.watcher_user_ids = REDMINE_IDS['WATCHERS']
+        issue.custom_fields= [
+        {'id': REDMINE_IDS['WEB_FINDING']['IDENTIFIER'], 'value': vuln.id},
+        {'id': REDMINE_IDS['WEB_FINDING']['DOMAIN'], 'value': vuln.domain},
+        {'id': REDMINE_IDS['WEB_FINDING']['RESOURCE'], 'value': vuln.target},
+        {'id': REDMINE_IDS['WEB_FINDING']['DATE_FOUND'], 'value': str(vuln.time.strftime("%Y-%m-%d"))},
+        {'id': REDMINE_IDS['WEB_FINDING']['LAST_SEEN'], 'value': str(vuln.time.strftime("%Y-%m-%d"))},
+        {'id': REDMINE_IDS['WEB_FINDING']['CVSS_SCORE'], 'value': vuln.cvss},
+        {'id': REDMINE_IDS['WEB_FINDING']['KB_DESCRIPTION'], 'value': str(vuln.observation.observation_title)},
+        {'id': REDMINE_IDS['WEB_FINDING']['KB_DESCRIPTION_NOTES'], 'value': str(vuln.observation.observation_note)},
+        {'id': REDMINE_IDS['WEB_FINDING']['KB_IMPLICATION'], 'value': str(vuln.observation.implication)},
+        {'id': REDMINE_IDS['WEB_FINDING']['KB_RECOMMENDATION'], 'value': str(vuln.observation.recommendation_title)},
+        {'id': REDMINE_IDS['WEB_FINDING']['KB_RECOMMENDATION_NOTES'], 'value': str(vuln.observation.recommendation_urls)}
+        ]
+        issue.uploads = vuln.attachments
         issue.save()
     except Exception as e:
-        print("Redmine error" + str(e))
+        print("Redmine error: " + str(e))
 
 def create_new_infra_issue(vuln):
     if redmine_client is None:
@@ -172,44 +172,44 @@ def create_new_code_issue(vuln):
         return
     if issue_already_exists(vuln):
         return
-    priority_dict = {'INFORMATIONAL': REDMINE_IDS['SEVERITY']['INFORMATIONAL'],
-         'LOW': REDMINE_IDS['SEVERITY']['LOW'], 'MEDIUM': REDMINE_IDS['SEVERITY']['MEDIUM'],
-          'HIGH': REDMINE_IDS['SEVERITY']['HIGH'], 'CRITICAL': REDMINE_IDS['SEVERITY']['CRITICAL']}
-    priority_id = priority_dict[vuln['observation']['severity'].upper()]
-    timestamp = datetime.now()
-    issue = redmine_client.issue.new()
-    issue.project_id = settings['REDMINE']['project_name']
-    issue.subject = vuln['Title']
-    issue.tracker_id = REDMINE_IDS['CODE_FINDING']['FINDING_TRACKER']
-    issue.description = vuln['Description']
-    issue.status_id = REDMINE_IDS['STATUS_NEW']
-    issue.priority_id = priority_id
-    issue.assigned_to_id = REDMINE_IDS['ASSIGNED_USER']
-    issue.watcher_user_ids = REDMINE_IDS['WATCHERS']
-    issue.custom_fields= [
-    {'id': REDMINE_IDS['CODE_FINDING']['IDENTIFIER'], 'value': vuln['_id']},
-    {'id': REDMINE_IDS['CODE_FINDING']["COMPONENT"], 'value': vuln['Component']},
-    {'id': REDMINE_IDS['CODE_FINDING']["LINE"], 'value': vuln['Line']},
-    {'id': REDMINE_IDS['CODE_FINDING']["AFFECTED_CODE"], 'value': vuln['Affected_code']},
-    {'id': REDMINE_IDS['CODE_FINDING']["FIRST_COMMIT"], 'value': vuln['Commit']},
-    {'id': REDMINE_IDS['CODE_FINDING']["LAST_COMMIT"], 'value': vuln['Commit']},
-    {'id': REDMINE_IDS['CODE_FINDING']["USERNAME"], 'value': vuln['Username']},
-    {'id': REDMINE_IDS['CODE_FINDING']["PIPELINE_NAME"], 'value': vuln['Pipeline_name']},
-    {'id': REDMINE_IDS['CODE_FINDING']["BRANCH_NAME"], 'value': vuln['Branch']},
-    {'id': REDMINE_IDS['CODE_FINDING']["TOOL_SEVERITY"], 'value':vuln['Severity_tool']},
-    {'id': REDMINE_IDS['CODE_FINDING']['DATE_FOUND'], 'value': str(timestamp.strftime("%Y-%m-%d"))},
-    {'id': REDMINE_IDS['CODE_FINDING']['LAST_SEEN'], 'value': str(timestamp.strftime("%Y-%m-%d"))},
-    {'id': REDMINE_IDS['CODE_FINDING']['CVSS_SCORE'], 'value': vuln['cvss_score']},
-    {'id': REDMINE_IDS['CODE_FINDING']['KB_DESCRIPTION'], 'value': vuln['observation']['observation_title']},
-    {'id': REDMINE_IDS['CODE_FINDING']['KB_DESCRIPTION_NOTES'], 'value': vuln['observation']['observation_note']},
-    {'id': REDMINE_IDS['CODE_FINDING']['KB_IMPLICATION'], 'value': vuln['observation']['implication']},
-    {'id': REDMINE_IDS['CODE_FINDING']['KB_RECOMMENDATION'], 'value': vuln['observation']['recommendation_title']},
-    {'id': REDMINE_IDS['CODE_FINDING']['KB_RECOMMENDATION_NOTES'], 'value': vuln['observation']['recommendation_note']}
-    ]
     try:
+        priority_dict = {'INFORMATIONAL': REDMINE_IDS['SEVERITY']['INFORMATIONAL'],
+             'LOW': REDMINE_IDS['SEVERITY']['LOW'], 'MEDIUM': REDMINE_IDS['SEVERITY']['MEDIUM'],
+              'HIGH': REDMINE_IDS['SEVERITY']['HIGH'], 'CRITICAL': REDMINE_IDS['SEVERITY']['CRITICAL']}
+        priority_id = priority_dict[vuln['observation']['severity'].upper()]
+        timestamp = datetime.now()
+        issue = redmine_client.issue.new()
+        issue.project_id = settings['REDMINE']['project_name']
+        issue.subject = vuln['Title']
+        issue.tracker_id = REDMINE_IDS['CODE_FINDING']['FINDING_TRACKER']
+        issue.description = vuln['Description']
+        issue.status_id = REDMINE_IDS['STATUS_NEW']
+        issue.priority_id = priority_id
+        issue.assigned_to_id = REDMINE_IDS['ASSIGNED_USER']
+        issue.watcher_user_ids = REDMINE_IDS['WATCHERS']
+        issue.custom_fields= [
+        {'id': REDMINE_IDS['CODE_FINDING']['IDENTIFIER'], 'value': vuln['_id']},
+        {'id': REDMINE_IDS['CODE_FINDING']["COMPONENT"], 'value': vuln['Component']},
+        {'id': REDMINE_IDS['CODE_FINDING']["LINE"], 'value': vuln['Line']},
+        {'id': REDMINE_IDS['CODE_FINDING']["AFFECTED_CODE"], 'value': vuln['Affected_code']},
+        {'id': REDMINE_IDS['CODE_FINDING']["FIRST_COMMIT"], 'value': vuln['Commit']},
+        {'id': REDMINE_IDS['CODE_FINDING']["LAST_COMMIT"], 'value': vuln['Commit']},
+        {'id': REDMINE_IDS['CODE_FINDING']["USERNAME"], 'value': vuln['Username']},
+        {'id': REDMINE_IDS['CODE_FINDING']["PIPELINE_NAME"], 'value': vuln['Pipeline_name']},
+        {'id': REDMINE_IDS['CODE_FINDING']["BRANCH_NAME"], 'value': vuln['Branch']},
+        {'id': REDMINE_IDS['CODE_FINDING']["TOOL_SEVERITY"], 'value':vuln['Severity_tool']},
+        {'id': REDMINE_IDS['CODE_FINDING']['DATE_FOUND'], 'value': str(timestamp.strftime("%Y-%m-%d"))},
+        {'id': REDMINE_IDS['CODE_FINDING']['LAST_SEEN'], 'value': str(timestamp.strftime("%Y-%m-%d"))},
+        {'id': REDMINE_IDS['CODE_FINDING']['CVSS_SCORE'], 'value': vuln['cvss_score']},
+        {'id': REDMINE_IDS['CODE_FINDING']['KB_DESCRIPTION'], 'value': vuln['observation']['observation_title']},
+        {'id': REDMINE_IDS['CODE_FINDING']['KB_DESCRIPTION_NOTES'], 'value': vuln['observation']['observation_note']},
+        {'id': REDMINE_IDS['CODE_FINDING']['KB_IMPLICATION'], 'value': vuln['observation']['implication']},
+        {'id': REDMINE_IDS['CODE_FINDING']['KB_RECOMMENDATION'], 'value': vuln['observation']['recommendation_title']},
+        {'id': REDMINE_IDS['CODE_FINDING']['KB_RECOMMENDATION_NOTES'], 'value': vuln['observation']['recommendation_note']}
+        ]
         issue.save()
     except Exception as e:
-        print("Redmine error" + str(e))
+        print("Redmine error: " + str(e))
 
 def create_new_web_issue_bis(vuln):
     if redmine_client is None:
